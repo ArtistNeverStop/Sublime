39974ad3fc2efe7a64c73a3eef101b6033976495
ArtistNeverStop <you@example.com>
2017-10-19 14:50:46 +0000

    Css Normalize, Skeleton.css, and Flexboxgrid, And Oauth2.0 innit with Passport

--- resources/assets/js/components/Global/Navbar.vue
+++ resources/assets/js/components/Global/Navbar.vue
@@ -1,6 +1,15 @@
 <template>
-  <nav class="navbar">
-    <h2>nav site</h2>
+  <nav class="container-fluid navbar">
+    <div class="row">
+      <div class="col-md-12">
+        <h2>nav site</h2>
+        <ul class="navbar-site-menu">
+          <li>
+            <a href="">Login</a>
+          </li>
+        </ul>
+      </div>
+    </div>
   </nav>
 </template>
 
