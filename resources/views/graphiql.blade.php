95b91deba4e3e77b42ca24e77d8d89eed2b4c9f9
Diego Meza <diego@agenciapradoja.com>
2017-11-26 14:31:09 -0600

    Default Query on GraphiQL added

--- resources/views/graphiql.blade.php
+++ resources/views/graphiql.blade.php
@@ -33,7 +33,51 @@
 
             // Parse the search string to get url parameters.
             var search = window.location.search;
-            var parameters = {};
+            var parameters = {
+                query: `# Welcome to GraphiQL
+#
+# GraphiQL is an in-browser tool for writing, validating, and
+# testing GraphQL queries.
+#
+# Type queries into this side of the screen, and you will see intelligent
+# typeaheads aware of the current GraphQL type schema and live syntax and
+# validation errors highlighted within the text.
+#
+# GraphQL queries typically start with a "{" character. Lines that starts
+# with a # are ignored.
+#
+# An example GraphQL query might look like:
+#
+#     {
+#       field(arg: "value") {
+#         subField
+#       }
+#     }
+#
+# Keyboard shortcuts:
+#
+#       Run Query:  Ctrl-Enter (or press the play button above)
+#
+#   Auto Complete:  Ctrl-Space (or just start typing)
+#
+
+query Example {
+  me {
+    id,
+    name
+  }
+}
+
+
+query users {
+  users {
+    id,
+    name
+  }
+}
+
+                `
+            };
             search.substr(1).split('&').forEach(function (entry) {
                 var eq = entry.indexOf('=');
                 if (eq >= 0) {
